f95c725ad19c5d25ac93511f267f181e6f77fad1
Owahed <owahedhossain@gmail.com>
2021-06-05 12:32:01 +0600

    df

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ for (; i < sL; i++) {
     if (str.charAt(i) === str.charAt(i).toLowerCase()) {
         console.log('uppercase:',str.charAt(i));
     }if (str.charAt(i) === str.charAt(i).toUpperCase()){
-        console.log('uppercase:',str.charAt(i));
+        // console.log('uppercase:',str.charAt(i));
     }
 }
